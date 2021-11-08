d6f1c8c7d438949be72bf95dca7f6948a368be26
jrughani <93567349+jrughani@users.noreply.github.com>
2021-11-08 14:00:45 -0500

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { VerifyEmailComponent } from './verify-email/verify-email.component';
+
+import { AuthGuard } from './services/auth.guard';
+
+
+const routes: Routes = [
+
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'verify-email', component: VerifyEmailComponent },
+  { path: '**', component: DashboardComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
